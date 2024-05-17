b921c829f3479691524f4f044696927ffa0467e3
David <alv.ricardodavid@gmail.com>
2024-05-17 20:29:33 -0300

    Seteo de idioma global y manualmente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -7,6 +7,13 @@ import { AppComponent } from './app.component';
 
 import { SharedModule } from './shared/shared.module';
 
+// Configuracion del locale de la app
+import localeEs from "@angular/common/locales/es-AR";
+import localeFr from "@angular/common/locales/fr-CA";
+import { registerLocaleData } from "@angular/common";
+
+registerLocaleData(localeEs);
+registerLocaleData(localeFr);
 
 @NgModule({
   declarations: [
@@ -18,7 +25,8 @@ import { SharedModule } from './shared/shared.module';
     BrowserAnimationsModule,
     SharedModule
   ],
-  providers: [],
+  providers: [{ provide: LOCALE_ID, useValue: 'es-AR'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
