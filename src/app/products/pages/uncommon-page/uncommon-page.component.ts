2dd9560646943dd8c469a7ee690e6e5c3b532767
David <alv.ricardodavid@gmail.com>
2024-05-18 16:36:07 -0300

    i18nPluralPipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -3,21 +3,56 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-uncommon-page',
   templateUrl: './uncommon-page.component.html',
-  styleUrl: './uncommon-page.component.css'
+  styleUrl: './uncommon-page.component.css',
 })
 export class UncommonPageComponent {
-
   public name: string = 'Ricardo';
-  public gender: 'male'|'female' = 'male';
+  public gender: 'male' | 'female' = 'male';
+
+  // i18nSelect
 
   public invitationMap = {
-    'male': 'invitarlo',
-    'female': 'invitarla'
+    male: 'invitarlo',
+    female: 'invitarla',
+  };
+
+  changeClient(): void {
+    if (this.name == 'Ricardo') {
+      this.name = 'Melisa';
+      this.gender = 'female';
+    } else {
+      this.name = 'Ricardo';
+      this.gender = 'male';
+    }
   }
 
-  changeClient():void {
-      this.name = 'Melisa';
-      this.gender = 'female'
+  // i18nPlural
+
+  public clients: string[] = [
+    'Maria',
+    'Pedrio',
+    'David',
+    'Laura',
+    'Francesca',
+    'Belen',
+    'Eduardo',
+    'Melisa',
+    'Natalia',
+    'Claudia',
+    'Ricardo',
+  ];
+
+  public clientsMap = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    '=2': 'tenemos 2 personas esperando.',
+    'other': 'tenemos # clientes esperando.'
   }
+
+  deleteClient():void {
+    this.clients.shift();
+  }
+
+
 
 }
