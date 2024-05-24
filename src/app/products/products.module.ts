735f739d2fcab2a168d642a803a575e4f83748ec
David <alv.ricardodavid@gmail.com>
2024-05-24 20:31:23 -0300

    Implements TootleCasePipe - Pipe Personalizado

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -6,6 +6,8 @@ import { BasicsPageComponent } from './pages/basics-page/basics-page.component';
 import { NumbersPageComponent } from './pages/numbers-page/numbers-page.component';
 import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
+import { OrderComponent } from './pages/order/order.component';
+import { ToggleCasePipe } from './pipes/toggle-case.pipe';
 
 
 @NgModule({
@@ -13,7 +15,8 @@ import { PrimeNgModule } from '../prime-ng/prime-ng.module';
     BasicsPageComponent,
     NumbersPageComponent,
     UncommonPageComponent,
-
+    OrderComponent,
+    ToggleCasePipe,
   ],
   imports: [
     CommonModule,
